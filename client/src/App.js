75a649ea3f2ad969f1c48e25091b9759427b21ef
maurcjnr <mauriciojunior.dev@gmail.com>
2021-04-06 13:50:17 -0300

    Starting project

---
+++ client/src/App.js
@@ -0,0 +1,10 @@
+import React from "react";
+import { Switch, Route, BrowserRouter as Router } from "react-router-dom";
+
+export default (props) => {
+  return (
+    <Switch>
+      <Route exact path="/" component={}></Route>
+    </Switch>
+  );
+};
